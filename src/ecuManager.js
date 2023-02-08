a1f88188e7d7300153178422d01038d923650d61
geoff <gesso15@gmail.com>
2023-02-07 16:55:10 -0800

    Use new data map and catch up to head
    also some new keys in OI

--- src/ecuManager.js
+++ src/ecuManager.js
@@ -2,7 +2,7 @@ import { performance } from "perf_hooks";
 import racePackDecoder from "./CAN/racepakDecoder.js";
 import openInverterDecoder from "./CAN/openInverterDecoder.js";
 import an400Decoder from "./CAN/an400Decorder.js";
-import { DATA_KEYS, WARNING_KEYS } from "./dataKeys.js";
+import { DATA_MAP, WARNING_KEYS } from "./dataKeys.js";
 import DataStore from "./DataStore.js";
 import RingBuffer from "./lib/ringBuffer.js";
 import ButtonManager from "./IO/Buttons.js";
@@ -14,7 +14,7 @@ export default (carSettings, canChannel) => {
     {
       onReleased: () => {
         gallonsLeft = carSettings.tank_size
-        ecuDataStore.write(DATA_KEYS.FUEL_LEVEL, 100)
+        ecuDataStore.write(DATA_MAP.FUEL_LEVEL, 100)
       },
       holdNeeded: true,
     },
@@ -22,8 +22,8 @@ export default (carSettings, canChannel) => {
       // light / dark theme toggle
       onPressed: () =>
         ecuDataStore.write(
-          DATA_KEYS.LOW_LIGHT_DETECTED,
-          ecuDataStore.read(DATA_KEYS.LOW_LIGHT_DETECTED) ? 0 : 1
+          DATA_MAP.LOW_LIGHT_DETECTED,
+          ecuDataStore.read(DATA_MAP.LOW_LIGHT_DETECTED) ? 0 : 1
         ),
       holdNeeded: false,
     },
@@ -43,6 +43,7 @@ export default (carSettings, canChannel) => {
       holdNeeded: false,
     },
   ]);
+  let getSpeed = () => 0;
   let msSample = 0;
   let lastMpgSampleTime = 0;
   let distance = 0;
@@ -61,12 +62,11 @@ export default (carSettings, canChannel) => {
    */
   const initializeFuel = (persistedGallonsLeft) => {
     gallonsLeft = persistedGallonsLeft;
-    ecuDataStore.updateWarning(WARNING_KEYS.ECU_COMM, true);
-    ecuDataStore.write(DATA_KEYS.FUEL_LEVEL, 0);
-    ecuDataStore.write(DATA_KEYS.AVERAGE_MPG, 0);
-    ecuDataStore.write(DATA_KEYS.CURRENT_MPG, 0);
-    ecuDataStore.write(DATA_KEYS.TEMP_TYPE, 0); // default to F
-    ecuDataStore.write(DATA_KEYS.PRESSURE_TYPE, 1); // default to kpa (used for MAP) / /make sure you front end gets what it expects!
+    ecuDataStore.write(DATA_MAP.FUEL_LEVEL, 0);
+    ecuDataStore.write(DATA_MAP.AVERAGE_MPG, 0);
+    ecuDataStore.write(DATA_MAP.CURRENT_MPG, 0);
+    msSample = performance.now();
+    lastMpgSampleTime = performance.now();
     updateFuelLeft();
   };
 
@@ -76,114 +76,131 @@ export default (carSettings, canChannel) => {
    */
   const initializeOdometer = (lastSavedReading) => {
     baseOdometerReading = lastSavedReading || carSettings.odometer;
-    ecuDataStore.write(DATA_KEYS.ODOMETER, baseOdometerReading);
+    ecuDataStore.write(DATA_MAP.ODOMETER, baseOdometerReading);
   };
+
+  const initializeSpeedo = () => {
+    if (carSettings.speedo === "GPS") {
+      getSpeed = () => ecuDataStore.read(DATA_MAP.GPS_SPEEED);
+    } else if (carSettings.speedo === "CAN") {
+      getSpeed = () => ecuDataStore.read(DATA_MAP.SPEEDO);
+    } else {
+      getSpeed = () => 0;
+    }
+  }
 
   const init = ({ gallonsLeft, odometer }) => {
     if (carSettings.buttons_enabled) {
       buttons.start(); // start listening for button presses
     }
     initializeFuel(gallonsLeft || carSettings.tank_size);
+    initializeSpeedo();
+    ecuDataStore.updateWarning(WARNING_KEYS.ECU_COMM, true);
+    ecuDataStore.write(DATA_MAP.TEMP_TYPE, 0); // default to F
+    ecuDataStore.write(DATA_MAP.PRESSURE_TYPE, 1); // default to kpa (used for MAP) / /make sure you front end gets what it expects!
     initializeOdometer(odometer);
-
-    msSample = performance.now();
-    lastMpgSampleTime = performance.now();
   };
 
+  
   const updateFuelLeft = () => {
     if (carSettings.fuel_level_enabled) {
       ecuDataStore.write(
-        DATA_KEYS.FUEL_LEVEL,
+        DATA_MAP.FUEL_LEVEL,
         Math.max(0, Math.ceil((gallonsLeft / carSettings.tank_size) * 100))
       );
     }
   }
 
-  const updateValue = ({ id, data }) => {
+  const updateMPG = (data) => {
+    if (getSpeed() < 0) {
+      return;
+    }
+    const newMsSample = performance.now();
+    const msDelta = newMsSample - msSample; // ms since last sample
+
+    //  calculate fuel consumption based on the last sample
+    const gpMs = (data * 0.1621) / 3600000; // convert from pounds/hour to gal/hour, then to to gal/millisecond
+    const pMin = Math.min(lastFuelSample, gpMs);
+    const gallonsConsumed =
+      (msDelta * (Math.max(lastFuelSample, gpMs) - pMin)) / 2 +  msDelta * pMin;
+
+    // update the fuel level
+    gallonsLeft -= gallonsConsumed;
+
+    // SPEED BASED DISTANCE - distance (m) = speed (m/millisecond) * time (ms)
+    // calculate distance since last sample
+    // we do this because the odometer is in mile denom; where as can get tiny slices of a mile traveled based on the speed and time
+    distance =
+      (getSpeed() / 3600000) * msDelta;
+
+    // calc average MPGs
+    const currentMpg = Math.floor(distance / gallonsConsumed);
+
+    // add a new sample every 10 seconds
+    if (newMsSample - lastMpgSampleTime > 10000) {
+      lastMpgSampleTime = newMsSample;
+      ecuDataStore.averageMPGPoints.push(mpgSampler.average);
+      ecuDataStore.write(
+        DATA_MAP.AVERAGE_MPG_POINT_INDEX,
+        ecuDataStore.averageMPGPoints.frontOffset
+      );
+      ecuDataStore.write(
+        DATA_MAP.AVERAGE_MPG,
+        ecuDataStore.averageMPGPoints.average
+      );
+      mpgSampler.reset();
+    } else {
+      mpgSampler.push(currentMpg);
+    }
+
+    ecuDataStore.write(DATA_MAP.CURRENT_MPG, currentMpg);
+    msSample = newMsSample;
+    lastFuelSample = gpMs;
+  }
+
+
+  const updateValue = ({ dataKey, data }) => {
     // do any special handling depending on the new updated value
-    switch (id) {
-      case DATA_KEYS.FUEL_FLOW:
-        const newMsSample = performance.now();
-        const msDelta = newMsSample - msSample; // ms since last sample
-
-        //  calculate fuel consumption based on the last sample
-        const gpMs = (data * 0.1621) / 3600000; // convert from pounds/hour to gal/hour, then to to gal/millisecond
-        const pMin = Math.min(lastFuelSample, gpMs);
-        const gallonsConsumed =
-          (msDelta * (Math.max(lastFuelSample, gpMs) - pMin)) / 2 +  msDelta * pMin;
-
-        // update the fuel level
-        gallonsLeft -= gallonsConsumed;
-
-        // SPEED BASED DISTANCE - distance (m) = speed (m/millisecond) * time (ms)
-        // calculate distance since last sample
-        // we do this because the odometer is in mile denom; where as can get tiny slices of a mile traveled based on the speed and time
-        distance =
-          (ecuDataStore.read(DATA_KEYS.GPS_SPEEED) / 3600000) * msDelta;
-
-        // calc average MPGs
-        const currentMpg = Math.floor(distance / gallonsConsumed);
-
-        // add a new sample every 10 seconds
-        if (newMsSample - lastMpgSampleTime > 10000) {
-          lastMpgSampleTime = newMsSample;
-          ecuDataStore.averageMPGPoints.push(mpgSampler.average);
-          ecuDataStore.write(
-            DATA_KEYS.AVERAGE_MPG_POINT_INDEX,
-            ecuDataStore.averageMPGPoints.frontOffset
-          );
-          ecuDataStore.write(
-            DATA_KEYS.AVERAGE_MPG,
-            ecuDataStore.averageMPGPoints.average
-          );
-          mpgSampler.reset();
-        } else {
-          mpgSampler.push(currentMpg);
-        }
-
-        ecuDataStore.write(DATA_KEYS.CURRENT_MPG, currentMpg);
+    switch (dataKey) {
+      case DATA_MAP.FUEL_FLOW:
+       
+        updateMPG(data);
         updateFuelLeft();
-
-        msSample = newMsSample;
-        lastFuelSample = gpMs;
         break;
-      case DATA_KEYS.CTS:
+      case DATA_MAP.CTS:
         ecuDataStore.updateWarning(
           WARNING_KEYS.ENGINE_TEMPERATURE,
           data > carSettings.engine_temp_high
         );
         break;
-      case DATA_KEYS.OIL_PRESSURE:
+      case DATA_MAP.OIL_PRESSURE:
         ecuDataStore.updateWarning(
           WARNING_KEYS.OIL_PRESSURE,
           data < carSettings.oil_low_limit
         );
         break;
-      case DATA_KEYS.BATT_VOLTAGE:
+      case DATA_MAP.BATT_VOLTAGE:
         ecuDataStore.updateWarning(
           WARNING_KEYS.BATT_VOLTAGE,
           data < carSettings.voltage_low_limit
         );
         break;
-      case DATA_KEYS.ODOMETER:
+      case DATA_MAP.ODOMETER:
         // the data represents the offset from our base odometer reading
         // if data is zero, that means the odometer reading has been reset to zero
         if (data === 0) {
           //update our base reading to be whatever we have stored in the current odometer
-          baseOdometerReading = ecuDataStore.read(DATA_KEYS.ODOMETER);
+          baseOdometerReading = ecuDataStore.read(DATA_MAP.ODOMETER);
         }
         data += baseOdometerReading;
         break;
+      case WARNING_KEYS.GPS_NOT_ACQUIRED:
+        if (data) ecuDataStore.write(DATA_MAP.GPS_SPEEED, -1);
       default:
         break;
     }
 
-    if (id < WARNING_KEYS.FIRST) {
-      // write data from CAN BUS  to store
-      ecuDataStore.write(id, data);
-    } else {
-      ecuDataStore.updateWarning(id, data);
-    }
+    ecuDataStore.update(dataKey, data);
   };
 
   /**
@@ -197,7 +214,7 @@ export default (carSettings, canChannel) => {
       // canparsing failure, shutdown
       return gpsUpdateStateToBroked(msg);
     }
-    msg.forEach((newData) => updateValue(newData));
+    msg.forEach((gpsData) => updateValue({dataKey: gpsData.id, data: gpsData.data}));
     return gpsUpdate;
   };
 
@@ -220,26 +237,29 @@ export default (carSettings, canChannel) => {
    * @returns {Function}
    */
   const gpsUpdateStateToBroked = (msg) => {
+    ecuDataStore.write(DATA_MAP.GPS_SPEEED, -1);
     ecuDataStore.updateWarning(WARNING_KEYS.GPS_ERROR, true);
     return gpsUpdateStateToWorking;
   };
 
   /** @type {Function} */
-  let gpsUpdater = gpsUpdate;
+  let gpsUpdater = gpsUpdateStateToBroked;
 
   const canUpdate = (msg) => {
     if (msg === false) {
       // canparsing failure, shutdown
       return canUpdateToError();
     } else {
-      decoder.do(msg).forEach((newData) => updateValue(newData));
+      decoder.do(msg).forEach((canData) => updateValue({dataKey: canData.id, data: canData.data}));
       return canUpdate;
     }
   };
 
   // turns on CAN error, initiate shutdown
   const canUpdateToError = () => {
-    // if (canChannel == 'can0') piShutdown.start(); // dont shutdown if we are stesting stuff
+    if (canChannel == 'can0' && carSettings.shutdown_when_can_stops) {
+      piShutdown.start();  // dont shutdown if we are stesting stuff
+    }
     ecuDataStore.updateWarning(WARNING_KEYS.ECU_COMM, true);
     return canUpdateErrorState;
   };
@@ -273,10 +293,10 @@ export default (carSettings, canChannel) => {
      *
      * @returns {Buffer}
      */
-    latestPacket: () => ecuDataStore.buffer,
+    latestPacket: () => ecuDataStore.buffer, // see todo in DataStore.js:write
     persistantData: () => {
       return {
-        odometer: ecuDataStore.read(DATA_KEYS.ODOMETER),
+        odometer: ecuDataStore.read(DATA_MAP.ODOMETER),
         gallonsLeft: gallonsLeft,
       };
     },
